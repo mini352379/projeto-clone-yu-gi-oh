0c7b5a3c6f2936bc66ae9498ff94892c9be89003
mini352379 <ebookandcurse2.0@gmail.com>
2025-01-27 20:06:51 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,49 @@
-const btnAvancar = document.getElementById("btn-avancar");
-const btnVoltar = document.getElementById("btn-voltar");
-const cartoes = document.querySelectorAll(".cartao");
+const btnAvancar = document.getElementById('btn-avancar')
+const btnVoltar = document.getElementById('btn-voltar')
+const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
 
-btnAvancar.addEventListener("click", function () {
+cartoes.forEach(cartao => {
+  cartao.addEventListener('click', function() {
+   const cartaVirada = cartao.querySelector('.carta-virada');
+
+   // virar o cartão
+   cartao.classList.toggle('virar');
+   //mostrar o fundo da carta
+   cartaVirada.classList.toggle('mostra-fundo-carta')
+
+   const descricao = cartao.querySelector('.descricao');
+   descricao.classList.toggle('esconder');
+  })
+});
+
+btnAvancar.addEventListener('click', function () {
   if (cartaoAtual === cartoes.length - 1) return;
 
+
   esconderCartaoSelecionado();
+
 
   cartaoAtual++;
   mostrarCartao(cartaoAtual);
 });
 
-btnVoltar.addEventListener("click", function () {
+btnVoltar.addEventListener('click', function () {
   if (cartaoAtual === 0) return;
 
+
   esconderCartaoSelecionado();
+
 
   cartaoAtual--;
   mostrarCartao(cartaoAtual);
 });
 
 function mostrarCartao(cartaoAtual) {
-  cartoes[cartaoAtual].classList.add("selecionado");
+  cartoes[cartaoAtual].classList.add('selecionado');
 }
 
 function esconderCartaoSelecionado() {
-  const cartaoSelecionado = document.querySelector(".selecionado");
-  cartaoSelecionado.classList.remove("selecionado");
+  const carataoSelecionado = document.querySelector('.selecionado');
+  carataoSelecionado.classList.remove('selecionado');
 }
